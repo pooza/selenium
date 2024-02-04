d065dc9e3990874ef6b4341cff5da6b2b2c6ca88
pooza <tkoishi@b-shock.co.jp>
2024-02-04 16:39:59 +0900

    .

--- lib/selenium/webdriver/common/logger.rb
+++ lib/selenium/webdriver/common/logger.rb
@@ -181,7 +181,7 @@ module Selenium
       private
 
       def create_logger(name, level:)
-        logger = ::Logger.new($stderr)
+        logger = ::Logger.new($stdout)
         logger.progname = name
         logger.level = level
         logger.formatter = proc do |severity, time, progname, msg|
