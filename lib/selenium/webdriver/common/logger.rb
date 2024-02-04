3bdfb8c41cbda29608f1b64877b58392ce766c24
pooza <tkoishi@b-shock.co.jp>
2024-02-04 16:49:50 +0900

    .

--- lib/selenium/webdriver/common/logger.rb
+++ lib/selenium/webdriver/common/logger.rb
@@ -181,7 +181,7 @@ module Selenium
       private
 
       def create_logger(name, level:)
-        logger = ::Logger.new($stdout)
+        logger = ::Logger.new($stderr)
         logger.progname = name
         logger.level = level
         logger.formatter = proc do |severity, time, progname, msg|
@@ -208,7 +208,7 @@ module Selenium
         msg = id.empty? ? message : "[#{id.map(&:inspect).join(', ')}] #{message} "
         msg += " #{yield}" if block_given?
 
-        @logger.send(level) { msg }
+        # @logger.send(level) { msg }
       end
     end # Logger
   end # WebDriver
